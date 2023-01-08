c8281b8c75c769ec047400647692af7d8d6b7b04
Bhakti087 <gbhakti04@gmail.com>
2023-01-08 23:56:04 +0530

    Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg'
-import './App.css'
-
+import { Navbar } from './components/Navbar/Navbar'
 function App() {
   return (
     <div className='App'>
-      <h1>Yo</h1>
+      <Navbar />
     </div>
   )
 }
