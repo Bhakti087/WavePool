aed4aaaa92a91d833f326e7d987aeaea10090a23
Bhakti087 <gbhakti04@gmail.com>
2023-01-11 23:50:54 +0530

    Navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className='Navbar'>
+      <div className='logo'>
+        <Link to='/'>
+          <img src='https://icons8.com/icon/WBjVk3Xn9Kju/krishna' />
+          <h1>Krsna</h1>
+        </Link>
+
+        <NavLink to='/About'>
+          <h3>About</h3>
+        </NavLink>
+
+        <NavLink to='/Activities'>
+          <h3>Activities</h3>
+        </NavLink>
+
+        <NavLink to='/Activities'>
+          <h3>Activities</h3>
+        </NavLink>
+
+        <NavLink to='/Activities'>
+          <h3>Activities</h3>
+        </NavLink>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
