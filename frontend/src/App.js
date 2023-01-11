8d451149bcac2585fc125bb8def23d1868356cb2
Bhakti087 <gbhakti04@gmail.com>
2023-01-11 23:55:14 +0530

    Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-// import { Navbar } from './components/Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar'
 import Paths from './Routes/Paths'
 
 function App() {
   return (
     <div className='App'>
-      {/* <Navbar /> */}
+      <Navbar />
       <Paths />
     </div>
   )
