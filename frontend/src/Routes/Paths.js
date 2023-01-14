32ae43b1ded2726f4718c836e41ec012173873f0
Bhakti087 <gbhakti04@gmail.com>
2023-01-14 23:12:35 +0530

    Paths

--- frontend/src/Routes/Paths.js
+++ frontend/src/Routes/Paths.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from '../components/LandingPage/LandingPage'
 import SignUp from '../components/SignUp/SignUp'
+import Login from '../components/Login/Login'
 const Paths = () => {
   return (
     <Routes>
       <Route path='/' element={<LandingPage />} />
       <Route path='/SignUp' element={<SignUp />} />
+      <Route path='/Login' element={<Login />} />
     </Routes>
   )
 }
